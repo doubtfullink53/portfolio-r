6e160d8e2881b83af8ccb61c7e674a3dbebb7dd2
Robert Mathews <doubtfullink53@gmail.com>
2022-09-01 19:51:26 -0400

    add portfolio section photos and links

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         navigation: true,
         autoScrolling:true,
-	    scrollHorizontally: true
-    });    
+	    scrollHorizontally: true,
+    });  
+    
+    
 });
+
+
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'maxWidth': 700,
+  })
