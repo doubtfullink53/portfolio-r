9ad10fdcf4e0cbac23d3cff1ac903c7e75cdc93f
Robert Mathews <doubtfullink53@gmail.com>
2022-09-02 19:13:59 -0400

    index.js add mobile onclick and onload click on mobile to make bigger than click link. On desktop scales on hover

--- index.js
+++ index.js
@@ -11,6 +11,38 @@ $(document).ready(function() {
 });
 
 
+function addLinkDesktop(){
+    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
+        
+    if (!isMobile){
+        document.getElementById("addLinkGitHubMobile").innerHTML = '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.2);"></i></a>'
+
+        document.getElementById("AddLinkEmailMobile").innerHTML = '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.2);"></i></a>'
+    }
+
+}
+
+
+
+
+
+ function addLinkGitHubMobile() {
+    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
+        
+    if(isMobile){
+        document.getElementById("addLinkGitHubMobile").innerHTML = '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.5);"></i></a>'
+    }
+   
+}
+
+function addLinkEmailMobile() {
+    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
+    if(isMobile){
+        document.getElementById("AddLinkEmailMobile").innerHTML = '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.3);"></i></a>'
+    }
+    
+   
+}
 
 
 
