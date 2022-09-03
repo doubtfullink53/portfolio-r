bf066e9b84384b33063d2417ac91f0868fe9a23e
Robert Mathews <doubtfullink53@gmail.com>
2022-09-02 20:05:02 -0400

    vanta.js animation to first section birds animation

--- index.js
+++ index.js
@@ -1,56 +1,49 @@
-
-
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        navigation: true,
-        autoScrolling:true,
-	    scrollHorizontally: true,
-    });  
-    
-    
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    navigation: true,
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
 });
 
+// functions to change
+function addLinkDesktop() {
+  var isMobile =
+    Math.min(window.screen.width, window.screen.height) < 768 ||
+    navigator.userAgent.indexOf("Mobi") > -1;
 
-function addLinkDesktop(){
-    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
-        
-    if (!isMobile){
-        document.getElementById("addLinkGitHubMobile").innerHTML = '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.2);"></i></a>'
+  if (!isMobile) {
+    document.getElementById("addLinkGitHubMobile").innerHTML =
+      '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.2);"></i></a>';
 
-        document.getElementById("AddLinkEmailMobile").innerHTML = '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.2);"></i></a>'
-    }
-
+    document.getElementById("AddLinkEmailMobile").innerHTML =
+      '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.2);"></i></a>';
+  }
 }
 
+function addLinkGitHubMobile() {
+  var isMobile =
+    Math.min(window.screen.width, window.screen.height) < 768 ||
+    navigator.userAgent.indexOf("Mobi") > -1;
 
-
-
-
- function addLinkGitHubMobile() {
-    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
-        
-    if(isMobile){
-        document.getElementById("addLinkGitHubMobile").innerHTML = '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.5);"></i></a>'
-    }
-   
+  if (isMobile) {
+    document.getElementById("addLinkGitHubMobile").innerHTML =
+      '<a href="https://github.com/doubtfullink53" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github" style="transform: scale(1.5);"></i></a>';
+  }
 }
 
 function addLinkEmailMobile() {
-    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
-    if(isMobile){
-        document.getElementById("AddLinkEmailMobile").innerHTML = '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.3);"></i></a>'
-    }
-    
-   
+  var isMobile =
+    Math.min(window.screen.width, window.screen.height) < 768 ||
+    navigator.userAgent.indexOf("Mobi") > -1;
+  if (isMobile) {
+    document.getElementById("AddLinkEmailMobile").innerHTML =
+      '<a href="mailto:2q97gb2lr@mozmail.com"><i class="fa-regular fa-envelope " style="transform: scale(1.3);"></i></a>';
+  }
 }
 
-
-
-
 lightbox.option({
-    'resizeDuration': 200,
-    'wrapAround': true,
-    'maxWidth': 700,
-  })
-
-
+  resizeDuration: 200,
+  wrapAround: true,
+  maxWidth: 700,
+});
